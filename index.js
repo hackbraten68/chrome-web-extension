a202fba2f17edd2076beb156db8c66398629a3ad
Sam Dillenburg <39334877+hackbraten68@users.noreply.github.com>
2021-05-29 15:17:05 +0200

    integrated delete button
    delete button clears localStorage, myLeads Array & listed Items in the extension

--- index.js
+++ index.js
@@ -2,24 +2,45 @@ let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
+const deleteBtn = document.getElementById("delete-btn")
+const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
+
+if (leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage
+    renderLeads()
+}
+
+deleteBtn.addEventListener("dblclick", function() {
+    console.log("button clicked")
+    // clear localStorage
+    localStorage.clear()
+    // clear myLeads
+    myLeads = []
+    // clear DOM -> listItems
+    listItems = []
+    renderLeads()
+})
+
+// 2. Listen for double clicks on the delete button (google it!)
+// 3. When clicked, clear localStorage, myLeads, and the DOM
 
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
     inputEl.value = ""
+    localStorage.setItem("myLeads", JSON.stringify(myLeads) )
     renderLeads()
 })
 
-
 function renderLeads() {
-let listItems = ""
-for (let i = 0; i < myLeads.length; i++) {
-    listItems += `
-        <li>
-            <a target='_blank' href='${myLeads[i]}'>
-                ${myLeads[i]}
-            </a>
-        </li>
-    `
-}
-ulEl.innerHTML = listItems
+    let listItems = ""
+    for (let i = 0; i < myLeads.length; i++) {
+        listItems += `
+            <li>
+                <a target='_blank' href='${myLeads[i]}'>
+                    ${myLeads[i]}
+                </a>
+            </li>
+        `
+    }
+    ulEl.innerHTML = listItems  
 }
